51e38bff3e85801246a3071b032ffa2bf6625e75
Aaron Santos <2302042@utrivieramaya.edu.mx>
2024-10-03 08:49:28 -0500

    New features: Nuevos cambios y modificaciones en los archivos
    En este caso se añadió código al archivo script.js
    Además de tener mas cambios en los archivos style.css y html
    En el index.html y en el style.css
    se añadieron nuevas clases

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* 
+Llamamos los métodos document.addEventListener junto con los parámetros 
+'DOMContentLoaded' y una función flecha llamada 
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    const navItems = document.querySelectorAll('.nav-item');
+
+    navItems.forEach((item, index) => {
+        setTimeout(() => {
+            item.classList.add('show');
+        }, 100 * index);
+    });
+
+});
